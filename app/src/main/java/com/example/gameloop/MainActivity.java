c7e9050b081d561e6523e7eb0a918cf4c333ce1a
Oli Knight <olidknight@gmail.com>
2022-04-03 20:54:41 +0100

    Create bottom nav bar

--- app/src/main/java/com/example/gameloop/MainActivity.java
+++ app/src/main/java/com/example/gameloop/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.gameloop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import com.example.gameloop.databinding.ActivityMainBinding;
+import com.example.gameloop.fragments.AllGenresFragment;
+import com.example.gameloop.fragments.HomeFragment;
+import com.example.gameloop.fragments.LatestFragment;
+import com.example.gameloop.fragments.PopularFragment;
 import com.example.gameloop.models.ApiResponse;
 import com.example.gameloop.models.Game;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.List;
 
@@ -23,11 +35,36 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment( new HomeFragment());
+        binding.nav.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment( new HomeFragment());
+                    break;
+                case R.id.popular:
+                    replaceFragment( new PopularFragment());
+                    break;
+                case R.id.latest:
+                    replaceFragment( new LatestFragment());
+                    break;
+                case R.id.all_genres:
+                    replaceFragment( new AllGenresFragment());
+                    break;
+
+            }
+
+            return true;
+        });
         String baseUrl = "https://api.rawg.io/api/";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
@@ -53,8 +90,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("FAIL", "Failed: " + t.getMessage());
             }
         });
-    }
 
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
     private void setAdapter( List<Game> gameList) {
         RecyclerAdapter adapter = new RecyclerAdapter(gameList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -63,4 +106,5 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
     }
+
 }
